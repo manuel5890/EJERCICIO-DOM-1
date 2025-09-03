f91f68b9edcc8ea317725798ad49f807ad3d444a
Manuel Calderon <manuelcalderon5890@gmail.com>
2025-09-03 15:44:00 -0500

    circulo dinamico con js

---
+++ js/main.js
@@ -0,0 +1,36 @@
+const figura = document.getElementById('figure');
+
+
+function circulo(){
+        figura.style.borderRadius = "50%";
+        figura.style.background="red"
+}
+
+
+
+
+
+
+
+
+
+
+document.addEventListener('DOMContentLoaded',function(){
+
+    document.getElementById('btnCircle').addEventListener('click' , circulo);
+    document.getElementById('btnStar').addEventListener('click' , estrella);
+    document.getElementById('btnCircle').addEventListener('click' , circulo);
+    document.getElementById('btnCircle').addEventListener('click' , circulo);
+    document.getElementById('btnCircle').addEventListener('click' , circulo);
+    document.getElementById('btnCircle').addEventListener('click' , circulo);
+    document.getElementById('btnCircle').addEventListener('click' , circulo);
+    document.getElementById('btnCircle').addEventListener('click' , circulo);
+    document.getElementById('btnCircle').addEventListener('click' , circulo);
+    document.getElementById('btnCircle').addEventListener('click' , circulo);
+    document.getElementById('btnCircle').addEventListener('click' , circulo);
+    document.getElementById('btnCircle').addEventListener('click' , circulo);
+    document.getElementById('btnCircle').addEventListener('click' , circulo);
+    document.getElementById('btnCircle').addEventListener('click' , circulo);
+    document.getElementById('btnCircle').addEventListener('click' , circulo);
+
+})
